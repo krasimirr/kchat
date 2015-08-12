e4f48996af4ce81baac36bcb85720f7ea3cb1107
krasimirr <zzzeek@abv.bg>
2015-08-12 22:46:19 +0300

    Update urls.py

--- krasimirchat/urls.py
+++ krasimirchat/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'krasimirchat.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('djangoChat.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^chat/', include('djangoChat.urls')),
 )
